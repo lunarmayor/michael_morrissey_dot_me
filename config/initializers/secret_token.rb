f0b134669d58efd4203a4b1862b0815ecd354430
Michael Morrissey <mikehmorrissey@gmail.com>
2013-10-21 16:37:35 -0500

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MichaelMorrisseyDotMe::Application.config.secret_key_base = '8153885701a3c445f96bba7e01c6a55fb1c6225a1a2026a0f327c9c1f2c357112fdf430f1d2ca334166e027ea5f1e46da4fa36ce3a4f6201ac130516699c794c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MichaelMorrisseyDotMe::Application.config.session_store :cookie_store, key: '_michael_morrissey_dot_me_session'
